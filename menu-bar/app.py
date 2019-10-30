e5323a299f7b96ec1366937bafacc97a4a750840
William Bell <williambell@bustermachine3.home>
2019-10-30 20:39:39 +0000

    Feature: menu bar example

---
+++ menu-bar/app.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Vte', '2.91')
+from gi.repository import Gtk, Vte, GLib
+
+class AppSignalHandlers:
+    def onDestroy(self, *args):
+        print("Destroy")
+    def onActivate(self, widget):
+        print("Select: ", widget.get_label())
+
+class Application(Gtk.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, application_id="com.genericsoft.app", **kwargs)
+        self.window = None
+        self.box = None
+        self.term = None
+    
+    def do_activate(self):
+        if not self.window:
+            # load glade layout
+            builder = Gtk.Builder()
+            builder.add_from_file("./window.glade")
+            # attach window to application
+            self.window = builder.get_object("window")
+            self.add_window(self.window)
+            # set signal handler
+            builder.connect_signals(AppSignalHandlers())
+
+            # show
+            self.window.show_all()
+
+if __name__ == "__main__":
+    print("Running")
+    app = Application()
+    app.run(sys.argv)
